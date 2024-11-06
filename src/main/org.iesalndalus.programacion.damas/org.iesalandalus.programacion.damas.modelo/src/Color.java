05c11a8e3220809f3faca5a65a38fa0f41d46a8d
David <davidramirezperez8@gmail.com>
2024-11-06 14:38:01 +0100

    Añadido el atributo cadenaAMostrar en el enum Color

--- src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Color.java
+++ src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Color.java
@@ -2,4 +2,13 @@ public enum Color {
     BLANCO ("Blanco"),
     NEGRO ("Negro");
 
+    private final String cadenaAMostrar;
+
+    private Color(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String getCadenaAMostrar() {
+        return cadenaAMostrar;
+    }
 }
