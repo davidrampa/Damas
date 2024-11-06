192bc741fc522e3c9ffe6f4f9647ee453c63682b
David <davidramirezperez8@gmail.com>
2024-11-06 15:45:50 +0100

    Añadir métodos get y set para los atributos fila y columna con validación de entrada.

--- src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Posicon.java
+++ src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Posicon.java
@@ -3,4 +3,26 @@ public class Posicon {
     private int fila;
     private char columna;
 
+    //Metodos Getters y Setters
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila < 1 || fila > 8) {
+            throw new IllegalArgumentException("La fila debe estar entra 1 y 8");
+        }
+        this.fila = fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+    public void setColumna(char columna) {
+        if (columna < 'a' || columna > 'h') {
+            throw new IllegalArgumentException("La columna debe estar entre 'a' y 'h'");
+        }
+        this.columna = columna;
+    }
 }
