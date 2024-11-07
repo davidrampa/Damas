9511c0da3ed685aaa1849243b0b624873c35d329
David <davidramirezperez8@gmail.com>
2024-11-07 13:06:55 +0100

    Implementado el metodo mover con las restricciones de movimiento para una dama

--- src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Dama.java
+++ src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Dama.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import javax.naming.OperationNotSupportedException;
 
 public class Dama {
 
@@ -51,6 +52,42 @@ public class Dama {
             columna = random.nextInt(8) + 1;
         } while ((fila + columna) % 2 == 0); //Asegura que este en una casilla negra
         return new Posicion(fila, columna);
+    }
+
+    public void mover(Direccion direccion, int pasos) throws  OperationNotSupportedException {
+        if (direccion == null) {
+            throw new IllegalArgumentException("La direccion no puede ser nula.");
+        }
+        if (pasos < 1) {
+            throw new IllegalArgumentException("El numero de pasos debe ser un entero positivo");
+        }
+        if (!esDamaEspecial) {
+            pasos = 1;
+            if ((color == Color.BLANCO && (direccion == Direccion.SURESTE || direccion == Direccion.SUROESTE)) ||
+                    (color == Color.NEGRO && (direccion == Direccion.NORESTE || direccion == Direccion.NOROESTE))) {
+                throw new OperationNotSupportedException("Movimiento no permite para damas no especiales.");
+            }
+        }
+
+        int nuevaFila = Posicion.getFila();
+        int nuevaColumna = Posicion.getColumna();
+
+        switch (direccion) {
+            case NORESTE -> { nuevaFila += pasos; nuevaColumna += pasos; }
+            case NOROESTE -> { nuevaFila += pasos; nuevaColumna += pasos; }
+            case SURESTE -> { nuevaFila -= pasos; nuevaColumna -= pasos; }
+            case SUROESTE -> { nuevaFila -= pasos; nuevaColumna -= pasos; }
+        }
+
+        if (nuevaFila < 1 || nuevaFila > 8 || nuevaColumna < 1 || nuevaColumna > 8) {
+            throw new OperationNotSupportedException("Movimiento fuera del tablero.");
+        }
+
+        Posicion = new Posicion(nuevaFila, nuevaColumna);
+
+        if ((color == Color.BLANCO && nuevaFila == 8) || (color == Color.NEGRO && nuevaFila == 1)) {
+            esDamaEspecial = true;
+        }
     }
 
 }
