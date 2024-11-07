6a6672646b6a00eb49a256f82647ce6c5db23a58
David <davidramirezperez8@gmail.com>
2024-11-07 11:51:18 +0100

    añadido metodos get y set para cada atributo en la clase Dama con validacion en los setters.

--- src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Dama.java
+++ src/main/org.iesalndalus.programacion.damas/org.iesalandalus.programacion.damas.modelo/src/Dama.java
@@ -3,4 +3,23 @@ public class Dama {
     private Color color;
     private Posicion posicion;
     private boolean esDamaEspecial;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        if (color == null) {
+            throw new IllegalArgumentException("El color no puede ser nulo");
+        }
+        this.color = color;
+    }
+
+    public boolean isEsDamaEspecial() {
+        return esDamaEspecial;
+    }
+
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
 }
