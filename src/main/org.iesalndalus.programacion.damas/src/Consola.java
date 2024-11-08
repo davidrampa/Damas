bfd3914a9184dd50447dcc2c4f405a46e66b848a
David <davidramirezperez8@gmail.com>
2024-11-08 11:33:57 +0100

    creado el metodo mostrarMenu en Consola.

--- src/main/org.iesalndalus.programacion.damas/src/Consola.java
+++ src/main/org.iesalndalus.programacion.damas/src/Consola.java
@@ -3,4 +3,12 @@ public class Consola {
     private Consola() {
 
     }
+
+    public static void mostrarMenu() {
+        System.out.println("Menu de Opciones:");
+        System.out.println("1. Crear dama por defecto");
+        System.out.println("2. Crear dama eligiendo el color");
+        System.out.println("3. Mover dama");
+        System.out.println("4. Salir");
+    }
 }
