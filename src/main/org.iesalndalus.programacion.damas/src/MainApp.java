5d612b1f059f91f5a30f6c67c0930ad9fee259fd
David <davidramirezperez8@gmail.com>
2024-11-08 13:45:31 +0100

    creado el metodo ejecutarOpcion

--- src/main/org.iesalndalus.programacion.damas/src/MainApp.java
+++ src/main/org.iesalndalus.programacion.damas/src/MainApp.java
@@ -1,3 +1,14 @@
 public class MainApp {
     private static Dama dama;
+
+    public static void ejecutarOpcion(int opcion) {
+        switch (opcion) {
+            case 1 -> crearDamaDefecto();
+            case 2 -> crearDamaColor();
+            case 3 -> mover();
+            case 4 -> Consola.despedirse();
+            default -> System.out.println("Opción no válida.");
+        }
+    }
+
 }
